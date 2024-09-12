62c0a0354f6418c3b2bd8400f28e316da12bbf72
quanhuynh11 <quan.m.huynh@edu.sait.ca>
2024-09-12 12:31:27 -0600

    Create student-info.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,5 +1,7 @@
 export default function Page() {
     return (
-
+        <main>
+            <h1>Shopping List</h1>
+        </main>
     );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,7 @@
+export default function StudentInfo() {
+    return (
+        <main>
+            <h2 class="text-2xl">Quan Huynh</h2>
+        </main>
+    );
+}
