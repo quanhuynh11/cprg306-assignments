0335a8f9798ba7520a25d9efdf6effbf34c3fcf0
quanhuynh11 <quan.m.huynh@edu.sait.ca>
2024-09-19 13:54:30 -0600

    final changes on week-3
    added <title>
    changed function name for page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,11 @@
 import HomeButton from "../_components/home-button";
 import ItemList from "./item-list";
 
-export default function WeekThreePage() {
+export default function Page() {
     return(
         <main className="p-10 bg-sky-950/55">
             <header className="">
+                <title>Shopping List</title>
                 <HomeButton />
                 <h1 className="text-4xl pt-10 font-bold">Shopping List</h1>
             </header>
